61a4cd9ec94d37d3fca2c23cd2227257aa7e0e7b
Pontus Ågren <h21ponag@du.se>
2023-05-22 16:20:27 +0200

    Added tests for Food class

--- TestSnakeGame/TestFood.cs
+++ TestSnakeGame/TestFood.cs
@@ -13,7 +13,69 @@ namespace TestSnakeGame
 {
     public class TestFood
     {
-       
+        [Fact]
+        public void Test_Food_FoodLocationShouldReturnFoodPos()
+        {
+            var food = new Food(new Position());
 
+            IPosition location = food.foodLocation();
+
+            var actual = location.ToString();
+
+            var expected = "10,10";
+
+            Assert.Equal(actual, expected);
+        }
+
+
+
+        [Fact]
+        public void Test_Food_FoodNewLocationShouldGenerateCorrectPosition()
+        {
+            Mock<ICanvas> mockCanvas = new Mock<ICanvas>();
+
+            mockCanvas.Setup(p => p.getWidth()).Returns(10);
+            mockCanvas.Setup(p => p.getHeight()).Returns(10);
+
+            Food food = new Food(new Position(), mockCanvas.Object);
+            food.foodNewLocation(new Mine(new Position(), mockCanvas.Object));
+
+            Assert.InRange(food._foodPos.x, 5, 10);
+            Assert.InRange(food._foodPos.y, 5, 10);
+        }
+
+        [Fact]
+        public void Test_Food_foodNewLocationShouldBeCalledTwice()
+        {
+            Mock<ICanvas> mockCanvas = new Mock<ICanvas>();
+
+            mockCanvas.Setup(p => p.getWidth()).Returns(10);
+            mockCanvas.Setup(p => p.getHeight()).Returns(10);
+
+            Mine mine = new Mine(new Position(), mockCanvas.Object);
+
+            Mock<IFood> food = new Mock<IFood>();
+
+            food.SetupAllProperties();
+
+            int timesCalled = 0;
+
+            mine.mineList.Add(new Dictionary<int, int>());
+            mine.mineList.Add(new Dictionary<int, int>());
+            mine.mineList[0].Add(0, 10);
+            mine.mineList[1].Add(0, 10);
+
+            food.Setup(p => p.foodNewLocation(mine)).Callback(() =>
+            {
+                if (mine.mineList[0][0] == mockCanvas.Object.getWidth() && mine.mineList[1][0] == mockCanvas.Object.getHeight())
+                {
+                    timesCalled++;
+                }
+            });
+
+            food.Object.foodNewLocation(mine);
+
+            Assert.NotEqual(0, timesCalled);
+        }
     }
 }
